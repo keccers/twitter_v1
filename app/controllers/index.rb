f76abcecae2f838785caa2c987ac987688779b63
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-29 11:02:08 -0500

    create Tweet and TwitterUser models and logic save tweets to db

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,24 @@ get '/' do
   erb :'index'
 end
 
+post '/get-tweets' do
+  @user = TwitterUser.find_or_create_by_username(params[:username])
+
+  if @user.tweets.empty?
+    # User#fetch_tweets! should make an API call
+    # and populate the tweets table
+    #
+    # Future requests should read from the tweets table 
+    # instead of making an API call
+    @user.fetch_tweets!
+  end
+
+  redirect "/#{params[:username]}"
+end
+
 get '/:username' do
-  erb :'username'
+  @user = TwitterUser.find_by_username(params[:username])
+  @tweets = @user.tweets.limit(10)
+  
+  erb :'user'
 end
